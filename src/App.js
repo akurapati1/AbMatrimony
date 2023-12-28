6ccf8dc5b6e140585a0839d6af7f00d0b1a09e9b
abhilash <abhilash.kurapati@slu.edu>
2023-12-28 12:33:15 -0600

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from "react";
 import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = () => {
+    setCurrentForm((prevForm) => (prevForm === 'login' ? 'register' : 'login'));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-side">
+        <h1 style={{ fontSize: '5em' }}>Welcome to AB Matrimony</h1>
+        <h3 fontSize="15px">Find your perfect match with us!</h3>
+      </div>
+      <div className="right-side">
+        <div className="form-box">
+          {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
