347cc4e45546cc74e9c881d83a6af1008150f6cd
abhilash <abhilash.kurapati@slu.edu>
2023-12-29 15:52:31 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -13,17 +13,33 @@ function App() {
 
   return (
     <div className="App">
+      <div className="navbar">
+        <ul>
+          <li><a href="#home" style={{fontFamily:'ubuntu', fontSize:'2em'}}>Home</a></li>
+          <li><a href="#about" style={{fontFamily:'ubuntu', fontSize:'2em'}}>About</a></li>
+          <li><a href="#contact" style={{fontFamily:'ubuntu', fontSize:'2em'}}>Contact Us</a></li>
+        </ul>
+      </div>
       <div className="left-side">
-        <h1 style={{ fontSize: '5em' }}>Welcome to AB Matrimony</h1>
-        <h3 fontSize="15px">Find your perfect match with us!</h3>
+        <h1 style={{
+          fontSize: '5em',
+          textAlign: 'left',
+          marginLeft: 50,
+          fontFamily: "ubuntu",
+          color: '#253031'
+        }}>Welcome to AB Matrimony</h1>
+        <h1 style={{
+          fontSize: '2em',
+          fontFamily: "ubuntu",
+          textAlign: 'right',
+          color: '#C6E0FF'
+        }}>-Find your perfect match with us!</h1>
       </div>
       <div className="right-side">
         <div className="form-box">
           {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
         </div>
-        
       </div>
-      
     </div>
   );
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -15,11 +15,11 @@ export const Register = (props) => {
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+            <input value={name} style={{backgroundColor: "lightcyan"}} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
             <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <input value={email} style={{backgroundColor: "lightcyan"}} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <input value={pass} style={{backgroundColor: "lightcyan"}} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <button type="submit">Register</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
